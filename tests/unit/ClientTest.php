92ef44875149b8b16396802ee67fd0c62cda6758
randler.ferraz <randler.ferraz@codificar.com.br>
2020-04-02 10:46:49 -0300

    adicionando teste exemplo

--- tests/unit/ClientTest.php
+++ tests/unit/ClientTest.php
@@ -3,6 +3,10 @@ namespace Delivery\Test;
 
 require_once ('vendor/autoload.php');
 
+/** Testes
+ * ./vendor/bin/phpunit --bootstrap vendor/autoload.php tests/unit/ClientTest.php 
+ */
+
 use Delivery\Client;
 use Delivery\Exceptions\DeliveryException;
 use Delivery\Endpoints\Endpoint;
@@ -47,6 +51,28 @@ final class ClientTest extends TestCase
         ),
         'return_to_start' => true,
     ];
+    private $optionRequestCreateStore = [
+        "person" => 1,
+        "user" => [
+            "name" => "Loja Teste",
+            "document" => "11122233344",
+            "email" => "teste@loja.com",
+            "phone" => "+5584988885555",
+            "password" => "qweqwe",
+            "acknowledgement" => "site",
+            "confirm_password" => "qweqwe"
+        ],
+        "address" => [
+            "street" => "Rua das ruas",
+            "zip_code" => "12312308",
+            "state" => "Estado",
+            "district" => "Bairro",
+            "city" => "Cidade",
+            "country" => "Brasil",
+            "complement" => "a",
+            "number" => "1"
+        ]
+    ];
     private $optionsRequestCreate = [
         "user_id" => 35,
         "token" => "2y10b3Zb8X03lI3qA0q3B170zuJDpQMOSJcykgrv2qK62OFsp3nIjYNee",
@@ -79,8 +105,18 @@ final class ClientTest extends TestCase
         )
     ];
 
+    public function testRequestTrackingSuccessfulResponse()
+    {
+        $client = new Client();
+
+        $response = $client->store()->create($this->optionRequestCreateStore);
+        //$response = $client->ride()->details(["id" => 162,"institution_id" => 1, "token" => '$2y$10$u8aWfSLtr3HwFUwrZXSZh.c5cda.rntSyUwEAbW2OEvGTDUGoaIn6']);
+        fwrite(STDERR, print_r($response));
+        
+    }
+
     
-    public function testEstimateSuccessfulResponse()
+/*    public function testEstimateSuccessfulResponse()
     {
         $client = new Client(null, true);
 
@@ -88,7 +124,7 @@ final class ClientTest extends TestCase
         
         $this->assertEquals($response->success, true);
     }
-
+*/
     /**
      * @expectException() \Delivery\Exceptions\DeliveryException
      */
@@ -173,12 +209,12 @@ final class ClientTest extends TestCase
         
     }
 */
-    public function testRequestTrackingSuccessfulResponse()
+    /*public function testRequestTrackingSuccessfulResponse()
     {
         $client = new Client(null, true);
 
         $response = $client->ride()->tracking(["id" => 2403, "api_key" => '$2y$10$aH0py6.UyUn2k9FTiDw28eLfXcNG0mEaJuy7Ib16gCKZ0NXNNgm9a']);
         fwrite(STDERR, print_r($response, TRUE));
         
-    }
+    }*/
 }
